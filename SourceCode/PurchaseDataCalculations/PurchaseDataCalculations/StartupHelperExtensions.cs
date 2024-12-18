dba01ab22b717bf3571e2f0e12604e11e6da90fe
Cristiano campos <cristianocampos_95@hotmail.com>
2024-12-18 22:19:40 +0000

    Review identation

--- SourceCode/PurchaseDataCalculations/PurchaseDataCalculations/StartupHelperExtensions.cs
+++ SourceCode/PurchaseDataCalculations/PurchaseDataCalculations/StartupHelperExtensions.cs
@@ -1,64 +1,63 @@
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using PurchaseData.ApplicationCore.BusinessServices;
 using PurchaseData.ApplicationCore.Factory;
 using PurchaseData.ApplicationCore.Interfaces;
 
-namespace PurchaseData.InterfaceLayer
+namespace PurchaseData.InterfaceLayer;
+
+internal static class StartupHelperExtensions
 {
-    internal static class StartupHelperExtensions
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
-        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        builder.Services.AddControllers(configure =>
         {
-            builder.Services.AddControllers(configure =>
-            {
-                configure.ReturnHttpNotAcceptable = true;
-            })
-            .AddNewtonsoftJson((options) =>
-            {
-                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
-            });
+            configure.ReturnHttpNotAcceptable = true;
+        })
+        .AddNewtonsoftJson((options) =>
+        {
+            options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+        });
 
-            builder.Services.AddLogging(logbuilder =>
-                logbuilder.AddFile(builder.Configuration.GetSection("FileLogging"))
-            );
+        builder.Services.AddLogging(logbuilder =>
+            logbuilder.AddFile(builder.Configuration.GetSection("FileLogging"))
+        );
 
-            builder.Services.AddScoped<IVatCalculationsService, VatCalculationsService>();
-            builder.Services.AddScoped<IVatRateServiceFactory, VatRateServiceFactory>();
-            builder.Services.AddScoped<IVatRateService, AustriaVatRateService>();
+        builder.Services.AddScoped<IVatCalculationsService, VatCalculationsService>();
+        builder.Services.AddScoped<IVatRateServiceFactory, VatRateServiceFactory>();
+        builder.Services.AddScoped<IVatRateService, AustriaVatRateService>();
 
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            return builder.Build();
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
-
-        public static WebApplication ConfigurePipeline(this WebApplication app)
+        else
         {
-            if (app.Environment.IsDevelopment())
+            // A custom exception Response when API is in production
+            app.UseExceptionHandler(appBuilder =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // A custom exception Response when API is in production
-                app.UseExceptionHandler(appBuilder =>
+                appBuilder.Run(async context =>
                 {
-                    appBuilder.Run(async context =>
-                    {
-                        context.Response.StatusCode = 500;
-                        await context.Response.WriteAsync(
-                            "An unexpected fault happened. Try again later.");
-                    });
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync(
+                        "An unexpected fault happened. Try again later.");
                 });
-            }
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            return app;
+            });
         }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
     }
 }
