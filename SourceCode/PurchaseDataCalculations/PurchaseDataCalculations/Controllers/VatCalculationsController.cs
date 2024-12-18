4f3bc2e458250ecef0fb45a9fd3e5599e8ae8acc
Cristiano campos <cristianocampos_95@hotmail.com>
2024-12-18 23:42:49 +0000

    Validate Vat rate via DI

--- SourceCode/PurchaseDataCalculations/PurchaseDataCalculations/Controllers/VatCalculationsController.cs
+++ SourceCode/PurchaseDataCalculations/PurchaseDataCalculations/Controllers/VatCalculationsController.cs
@@ -12,12 +12,14 @@ namespace PurchaseData.InterfaceLayer.Controllers
         private readonly ILogger<VatCalculationsController> _logger;
         private readonly IVatCalculationsService _service;
         private readonly IVatRateServiceFactory _rateServiceFactory;
+        private readonly IVatRateService _vatRateStrategy;
 
-        public VatCalculationsController(ILogger<VatCalculationsController> logger, IVatCalculationsService service, IVatRateServiceFactory rateServiceFactory)
+        public VatCalculationsController(ILogger<VatCalculationsController> logger, IVatCalculationsService service, IVatRateServiceFactory rateServiceFactory, IVatRateService vatRateStrategy)
         {
             _logger = logger;
             _service = service;
             _rateServiceFactory = rateServiceFactory;
+            _vatRateStrategy = vatRateStrategy;
         }
 
         [HttpGet]
@@ -25,12 +27,12 @@ namespace PurchaseData.InterfaceLayer.Controllers
         {
             try
             {
-                var vatRateStrategy = _rateServiceFactory.GetStrategy(_country);
-                if (!vatRateStrategy.IsValidVatRate(requestEntity.VatRate ?? 0))
+                //var vatRateStrategy = _rateServiceFactory.GetStrategy(_country);
+                if (!_vatRateStrategy.IsValidVatRate(requestEntity.VatRate ?? 0))
                 {
                     ModelState.AddModelError(
                         nameof(requestEntity.VatRate),
-                        $"Invalid VAT rate for {_country}. Accepted values are: {string.Join(", ", vatRateStrategy.GetVatRates())}"
+                        $"Invalid VAT rate for {_country}. Accepted values are: {string.Join(", ", _vatRateStrategy.GetVatRates())}"
                     );
 
                     return ValidationProblem();
